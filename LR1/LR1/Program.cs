04fa8eefd4962ee0374ad32fd5c6766f5c06fba3
gail64917 <gail64917@yandex.ru>
2017-09-11 05:02:48 +0300

    upd: Heroku в процессе

--- LR1/LR1/Program.cs
+++ LR1/LR1/Program.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 
 namespace LR1
 {
@@ -11,10 +12,12 @@ namespace LR1
     {
         public static void Main(string[] args)
         {
+            //var config = new ConfigurationBuilder().AddCommandLine(args).Build();
             var host = new WebHostBuilder()
                 .UseKestrel()
                 .UseContentRoot(Directory.GetCurrentDirectory())
                 .UseIISIntegration()
+                //.UseConfiguration(config)
                 .UseStartup<Startup>()
                 .UseApplicationInsights()
                 .Build();
