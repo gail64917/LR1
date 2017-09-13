519a04075e1e919d12bd68e2e9d2a6be5e33cd60
Vladimir <gail64917@yandex.ru>
2017-09-13 03:08:05 +0300

    upd: EF seting up

---
+++ LR1/Data/SchoolContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LR1.Models;
+
+namespace LR1.Data
+{
+    public class SchoolContext
+    {
+        //public SchoolContext(DbContextOptions<SchoolContext> options) : base(options)
+        //{
+        //}
+
+        //public DbSet<Course> Courses { get; set; }
+        //public DbSet<Enrollment> Enrollments { get; set; }
+        //public DbSet<Student> Students { get; set; }
+    }
+}
---
+++ XUnitTest/UnitTest1.cs
@@ -0,0 +1,15 @@
+using System;
+using Xunit;
+using LR1;
+
+namespace XUnitTest
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            
+        }
+    }
+}
